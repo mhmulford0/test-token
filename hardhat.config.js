8879272167c13d1ea15eeeddd6449c73b126ad76
Matthew Mulford <mhmulford0@gmail.com>
2021-09-26 17:08:27 -0500

    basic token setup

---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+require("@nomiclabs/hardhat-waffle");
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const privateKey = process.env.PRIVATE_KEY
+module.exports = {
+  solidity: "0.8.4",
+  paths: {
+    artifacts: './artifacts'
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/e5aa55e375464ff6b7bd71d4b31589d5",
+      accounts: [`0x${privateKey}`]
+    }
+  }
+};
--- next-env.d.ts
+++ next-env.d.ts
@@ -2,5 +2,9 @@
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
+interface Window {
+  ethereum: any;
+}
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
